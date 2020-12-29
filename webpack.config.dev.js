207ef03adee58334267d418104ad583f36a67905
Umma Ahimsha <uahimsha@gmail.com>
2020-12-29 12:54:51 +0700

    init

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,44 +1,44 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development';
 
 module.exports = {
-  mode: "development",
-  target: "web",
-  devtool: "cheap-module-source-map",
-  entry: "./src/index",
+  mode: 'development',
+  target: 'web',
+  devtool: 'cheap-module-source-map',
+  entry: './src/index',
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   devServer: {
-    stats: "minimal",
+    stats: 'minimal',
     overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      favicon: "src/favicon.ico"
-    })
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
