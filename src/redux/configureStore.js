8c51eb189c40ade0c789f822dbc825674dd25904
Umma Ahimsha <uahimsha@gmail.com>
2020-12-29 14:38:02 +0700

    renaming ReduxISI

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import reduxISI from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for redux dev tools
 
-  const enhancers = applyMiddleware(reduxImmutableStateInvariant());
+  const enhancers = applyMiddleware(reduxISI());
 
   return createStore(rootReducer, initialState, composeEnhancers(enhancers));
 }
