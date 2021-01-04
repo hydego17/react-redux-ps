a4aa3c8ac9d86c58d29d9b7a321fa5b0fd869709
Umma Ahimsha <uahimsha@gmail.com>
2021-01-04 13:52:32 +0700

    added API URL for DB

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,6 +23,9 @@ module.exports = {
     https: false,
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.API_URL': JSON.stringify('https://localhost:3001'),
+    }),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       favicon: 'src/favicon.ico',
