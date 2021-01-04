f5bcfb8b047fe81bfa4be3571f022155c7e90d68
Umma Ahimsha <uahimsha@gmail.com>
2021-01-04 15:25:06 +0700

    useColorModeValue for linkColor in courses table render

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -11,46 +11,50 @@ import {
   Td,
   Button,
   Link as ChakraLink,
+  useColorModeValue,
 } from '@chakra-ui/react';
 
-const CourseList = ({ courses }) => (
-  <Table variant="simple">
-    <Thead>
-      <Tr>
-        <Th />
-        <Th>Title</Th>
-        <Th>Author</Th>
-        <Th>Category</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      {courses.map((course) => {
-        return (
-          <Tr key={course.id}>
-            <Td>
-              <Button>
-                <a href={'http://pluralsight.com/courses/' + course.slug}>
-                  Watch
-                </a>
-              </Button>
-            </Td>
-            <Td>
-              <ChakraLink
-                as={ReactLink}
-                to={'/course/' + course.slug}
-                color="teal.600"
-              >
-                {course.title}
-              </ChakraLink>
-            </Td>
-            <Td>{course.authorName}</Td>
-            <Td>{course.category}</Td>
-          </Tr>
-        );
-      })}
-    </Tbody>
-  </Table>
-);
+const CourseList = ({ courses }) => {
+  const linkColor = useColorModeValue("teal.600", "teal.400")
+  return (
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th />
+          <Th>Title</Th>
+          <Th>Author</Th>
+          <Th>Category</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {courses.map((course) => {
+          return (
+            <Tr key={course.id}>
+              <Td>
+                <Button>
+                  <a href={'http://pluralsight.com/courses/' + course.slug}>
+                    Watch
+                  </a>
+                </Button>
+              </Td>
+              <Td>
+                <ChakraLink
+                  as={ReactLink}
+                  to={'/course/' + course.slug}
+                  color={linkColor}
+                >
+                  {course.title}
+                </ChakraLink>
+              </Td>
+              <Td>{course.authorName}</Td>
+              <Td>{course.category}</Td>
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+};
 
 CourseList.propTypes = {
   courses: PropTypes.array.isRequired,
