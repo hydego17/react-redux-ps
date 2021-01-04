e2a559e66a9c44222865974f38014e4b1f41d1a8
Umma Ahimsha <uahimsha@gmail.com>
2021-01-04 13:48:25 +0700

    formatting dcmnt

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './components/App';
-
-import { Provider as ReduxProvider } from 'react-redux';
-import configureStore from './redux/configureStore';
 
 const store = configureStore();
 
